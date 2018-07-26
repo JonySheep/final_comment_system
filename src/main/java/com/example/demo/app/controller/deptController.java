19806a1b2f86e770c80c2dbc9b1cd0ccdfd1ba9e
Yang <yyqyang222@163.com>
2018-07-26 20:03:05 +0800

    ！！！写了这么多天代码居然没有contribution！！生气！！！

--- src/main/java/com/example/demo/app/controller/deptController.java
+++ src/main/java/com/example/demo/app/controller/deptController.java
@@ -1,5 +1,6 @@
 package com.example.demo.app.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/dept")
 public class deptController {
 
+    @RequestMapping(value = "/{dept_id}")
+    public void getDept(@PathVariable(value = "dept_id") int deptID){
 
+    }
 }
