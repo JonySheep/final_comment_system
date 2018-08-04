40e4c8cb6dde4f7d13b0a830ba2b4172e00f545f
Yang <yyqyang222@163.com>
2018-08-04 15:37:27 +0800

    修改首页面

--- web/pages/index/index.js
+++ web/pages/index/index.js
@@ -4,10 +4,10 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {},
     hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    isFinish:false,
   },
   //事件处理函数
   bindViewTap: function() {
@@ -48,13 +48,53 @@ Page({
     app.globalData.userInfo = e.detail.userInfo
     this.setData({
       userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      hasUserInfo: true,
+      isLogin:true
     })
   },
 
+
+  /**
+   * 点击开始
+   */
   toStart:function(){
+    this.judge();
+    if(this.data.hasUserInfo){
+      this.addCommentPerson(this.data.userInfo.nickName);
+    }
+   
     wx.navigateTo({
       url: '../classification',
+    })
+  },
+
+
+  /**
+   * 记录已评价人的微信名称
+   */
+  addCommentPerson:function(name){
+    wx.request({
+      url: 'http://154.8.162.147:8080/addPerson/'+name,
+      method:POST,
+      data: {},
+      header: {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+    })
+  },
+
+
+  /**
+   * 判断该用户是否进行评价
+   */
+  judge:function(name){
+    let _this=this;
+    wx.request({
+      url: 'http://154.8.162.147:8080/judge/' + name,
+      method:GET,
+      success:function(){
+        _this.setData({hasUserInfo:false})
+      }
     })
   }
 })
